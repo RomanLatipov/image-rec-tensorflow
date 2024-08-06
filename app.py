cddaeac4b2155bf0b216f9f29397619285e29fe4
RomanLatipov <romanlatipov@gmail.com>
2024-08-05 23:38:44 -0400

    build the network

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import tensorflow as tf, numpy as np
 from matplotlib import pyplot as plt
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
 
 # print(tf.cofig.experimental.list_pysical_devices('GPU'))
 
@@ -37,3 +39,21 @@ test_size = int(len(data)*.1)+1
 train = data.take(train_size)
 val = data.skip(train_size).take(val_size)
 test = data.skip(train_size+val_size).take(test_size)
+
+model = Sequential()
+model.add(Conv2D(16, (3,3), 1, activation='relu', input_shape=(256,256,3)))
+model.add(MaxPooling2D())
+
+model.add(Conv2D(32, (3,3), 1, activation='relu'))
+model.add(MaxPooling2D())
+
+model.add(Conv2D(16, (3,3), 1, activation='relu'))
+model.add(MaxPooling2D())
+
+model.add(Flatten())
+
+model.add(Dense(256, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile('adam', loss=tf.losses.BinaryCrossentropy(), metrics=['accuracy'])
+model.summary()
